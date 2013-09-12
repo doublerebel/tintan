fb670d9bfb358a9b490e117d68bfb64a3997195c
Charles Phillips <charles@doublerebel.com>
2013-09-12 12:35:36 -0700

    Mixed tabs and spaces to spaces only

--- etc/plugins/tintan/plugin.py
+++ etc/plugins/tintan/plugin.py
@@ -15,38 +15,37 @@ except:
 # so we have to figure out which json functions to use.
 to_json = None
 if hasattr(json, 'dumps'):
-	to_json = json.dumps
+    to_json = json.dumps
 else:
-	to_json = json.write
+    to_json = json.write
 
 def compile(config):
-        c = config.copy()
-        c.pop('tiapp', '')
-        c.pop('android_builder', '')
-        c.pop('logger', '')
-        project_dir = c.get('project_dir')
+    c = config.copy()
+    c.pop('tiapp', '')
+    c.pop('android_builder', '')
+    c.pop('logger', '')
+    project_dir = c.get('project_dir')
 
-        tintan = os.path.join(project_dir, 'node_modules/tintan/bin/tintan')
+    tintan = os.path.join(project_dir, 'node_modules/tintan/bin/tintan')
 
-	print "[INFO] Executing Tintan"
-        print "       node", tintan, "-C", project_dir, 'tintan'
+    print "[INFO] Executing Tintan"
+    print "       node", tintan, "-C", project_dir, 'tintan'
 
-        proc = subprocess.Popen(['node', tintan, '-C', project_dir, 'tintan'],
-                                env={'TINTAN': to_json(c),
-                                     'PATH': os.environ['PATH'] + ':/usr/local/bin'},
-                                stderr = sys.stderr, stdout = sys.stdout)
-        proc.communicate();
-        ret = proc.wait()
+    proc = subprocess.Popen(['node', tintan, '-C', project_dir, 'tintan'],
+                            env={'TINTAN': to_json(c),
+                                 'PATH': os.environ['PATH'] + ':/usr/local/bin'},
+                            stderr = sys.stderr, stdout = sys.stdout)
+    proc.communicate();
+    ret = proc.wait()
 
-        if ret != 0:
-          raise Exception('Tintan terminated with exitcode: '+str(ret))
+    if ret != 0:
+        raise Exception('Tintan terminated with exitcode: '+str(ret))
 
 if __name__ == '__main__':
-	proj_dir = None
-	if len(sys.argv) < 2:
-		proj_dir = os.getcwd()
-	else:
-		proj_dir = sys.argv[1]
-	config = {'project_dir': proj_dir, 'cli': True, 'tiapp': None}
-        compile(config)
-
+    proj_dir = None
+    if len(sys.argv) < 2:
+        proj_dir = os.getcwd()
+    else:
+        proj_dir = sys.argv[1]
+    config = {'project_dir': proj_dir, 'cli': True, 'tiapp': None}
+    compile(config)
